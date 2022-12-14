a76ea3f8dd5d9ae6e12659e27f1905129b865fb4
samlee088 <108655429+samlee088@users.noreply.github.com>
2022-12-13 17:19:00 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const mongodb = require('mongodb').MongoClient;
+
+const app = express();
+const port = 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017/shelterDB`;
+
+let db;
+
+mongodb.connect(
+    connectionStringURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+        db = client.db();
+        app.listen(port, () => {
+            console.log(`App listening at http://localhost:${port}`);
+        })
+    }
+)
+
+
+app.use(express.json());
+
+/* This is where the mongodb routes will go */
+
+
+
+
+
